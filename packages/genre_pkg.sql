efcb3d14a26aa6b61b9f36306dc3cd4e4fa16a3f
isaqAbbas <59609513+isaqAbbas@users.noreply.github.com>
2020-01-26 22:46:06 +0400

    comment changed
    comment changed

--- packages/genre_pkg.sql
+++ packages/genre_pkg.sql
@@ -5,7 +5,7 @@ procedure upd(p_id genre.id%type,p_name genre.name%type);
 procedure del(p_id genre.id%type);
 end  genre_pkg;
 /
---create package body part
+--create package body parts
 create or replace package body genre_pkg as
 procedure ins(p_name genre.name%type) as
 begin
