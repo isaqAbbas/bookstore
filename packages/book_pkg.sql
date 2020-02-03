f226c77d265e27ab103b1629dc59612b72515c5f
isaqAbbas <59609513+isaqAbbas@users.noreply.github.com>
2020-02-03 12:38:48 +0400

    unique book_name and author_id
    unique book_name and author_id

--- packages/book_pkg.sql
+++ packages/book_pkg.sql
@@ -7,10 +7,31 @@ end book_pkg;
 /
 --create package body part
 create or replace package body book_pkg as
-procedure ins(p_name book.name%type,p_genre_id book.genre_id%type,p_author_id book.genre_id%type) as
+procedure ins(p_name book.name%type,p_genre_id book.genre_id%type,p_author_id book.genre_id%type) as 
+v_cnt number;
+--v_cnt2 number;
 begin
-insert into book(id,name,author_id,genre_id) values(book_seq.nextval,p_name,p_author_id,p_genre_id);
-commit;
+/*select count(*) into v_cnt2 from  book where author_id=p_author_id;
+if v_cnt2=0 then
+insert into book(id,name,author_id,genre_id) values(book_seq.nextval,p_name,p_author_id,p_genre_id); 
+else 
+  raise_application_error(-20001,'Muellif id/ni tekrar daxil etmek olmaz!'); 
+end if;
+*/
+  select count(*) into v_cnt from  book where name=p_name and author_id=p_author_id;
+  if v_cnt=0 then
+  insert into book(id,name,author_id,genre_id) values(book_seq.nextval,p_name,p_author_id,p_genre_id);
+  else
+  raise_application_error(-20001,'Eyni adli kitabi tekrar daxil etmek olmaz!'); 
+  end if;
+  /*
+  if p_genre_id is not  null and p_author_id is not  null then 
+    insert into book(id,name,author_id,genre_id) values(book_seq.nextval,p_name,p_author_id,p_genre_id);
+    commit;
+  else 
+    raise_application_error(-20001,'Janr ve ya Muellif daxil edilmayibdir');
+  end if;   
+  */
 end;
 procedure upd(p_id book.id%type,p_name book.name%type,p_genre_id book.genre_id%type,p_author_id book.author_id%type) as
 begin
