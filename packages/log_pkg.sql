e3d9c21edfd9e71b5c7db17949d46721d79d60ac
1sa9 <isaq.abbaszade@socar.az>
2020-02-23 00:17:24 +0400

    update log_pkg package
    update log_pkg package

--- packages/log_pkg.sql
+++ packages/log_pkg.sql
@@ -1,12 +1,16 @@
 create or replace  package log_pkg as
 procedure log(p_message logs.message%type,p_message_type logs.message_type%type);
 end log_pkg;
-
+/
 create or replace  package body log_pkg as
 procedure log(p_message logs.message%type,p_message_type logs.message_type%type) as
 pragma autonomous_transaction;
+
 begin
+  
 insert into logs values(p_message,p_message_type,sysdate);
 commit;
+
 end log;
+
 end log_pkg;
