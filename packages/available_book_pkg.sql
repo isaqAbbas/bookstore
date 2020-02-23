0f543449fe52652169803e3a97d27b6c5f49bfab
1sa9 <isaq.abbaszade@socar.az>
2020-02-23 04:51:13 +0400

    update package genre & available_book
    update package genre & available_book

--- packages/available_book_pkg.sql
+++ packages/available_book_pkg.sql
@@ -1,28 +1,29 @@
 create or replace package available_book_pkg as
-procedure ins(p_book_id available_book.book_id%type,p_store_id available_book.store_id%type,p_sold available_book.sold%type,p_sold_date available_book.sold_date%type);
+procedure ins(p_book_id available_book.book_id%type,p_store_id available_book.store_id%type);
 procedure upd(p_book_id available_book.book_id%type,p_store_id available_book.store_id%Type,p_sold available_book.sold%Type,p_sold_date available_book.sold_date%Type);
 procedure del(p_book_id available_book.book_id%type,p_store_id available_book.store_id%type,p_sold_date available_book.sold_date%type);
 end available_book_pkg;
 /
 create or replace  package body available_book_pkg as
 
-procedure ins(p_book_id available_book.book_id%type,p_store_id available_book.store_id%type,p_sold available_book.sold%type,p_sold_date available_book.sold_date%type) as
-v_cnt number;
+procedure ins(p_book_id available_book.book_id%type,p_store_id available_book.store_id%type) as
+--v_cnt number;
 
 begin
 
-if p_book_id is null or p_store_id is null or p_sold is null or p_sold_date is null  then
-log_pkg.log(p_message =>'Butun melumatlar daxil edilmelidir!!!',p_message_type =>'critical' );
+if p_book_id is null or p_store_id is null  then
+--log_pkg.log(p_message =>'Butun melumatlar daxil edilmelidir!!!',p_message_type =>'critical' );
 raise_application_error(-20001,'Butun melumatlar daxil edilmelidir!!!');
 end if;
 
-select count(*) into v_cnt from available_book where book_id =p_book_id and store_id=p_store_id and sold_date=p_sold_date;
+
+/*select count(*) into v_cnt from available_book where book_id =p_book_id and store_id=p_store_id and sold_date=p_sold_date;
 if v_cnt>0 then
 log_pkg.log(p_message =>'Kitab adi,magazani ve satilma tarixini tekrar daxil etmek olmaz!!!',p_message_type =>'noncritical' );
 raise_application_error(-20001,'Kitab adi,magazani ve satilma tarixini tekrar daxil etmek olmaz!!!');
-end if;
-
-insert into available_book(book_id,store_id,sold,sold_date) values(p_book_id,p_store_id,p_sold,p_sold_date);
+end if;*/
+if 
+insert into available_book(book_id,store_id,sold,sold_date) values(p_book_id,p_store_id,1,p_sold_date);
 commit;
 end ins;
 
--- packages/genre_pkg.sql
+++ packages/genre_pkg.sql
@@ -39,7 +39,7 @@ log_pkg.log(p_message =>'Melumati tekrar daxil etmek olmaz!!!' ,p_message_type =
 raise_application_error(-20001,'Melumati tekrar daxil etmek olmaz!!!');
 end if;
 
-update genre set name=p_name where id=p_id and name=p_name;
+update genre set name=p_name where id=p_id and name=p_name;---niye tekce id=p_id yox?
 commit;
 end upd;
 
