882657722f8f20d002d3d5e3bbe5a33bd39bc71b
isaqAbbas <isaq.abbaszade@socar.az>
2020-03-07 15:53:39 +0400

    Update available_book_pkg package
    Update available_book_pkg package

--- packages/available_book_pkg.sql
+++ packages/available_book_pkg.sql
@@ -1,56 +1,24 @@
 create or replace package available_book_pkg as
 procedure ins(p_book_id available_book.book_id%type,p_store_id available_book.store_id%type);
---procedure upd(p_book_id available_book.book_id%type,p_store_id available_book.store_id%Type,p_sold available_book.sold%Type,p_sold_date available_book.sold_date%Type);
 procedure del(p_book_id available_book.book_id%type,p_store_id available_book.store_id%type,p_all_books boolean default false);
-procedure sell(p_store_id available_book.store_id%type,p_book_id available_book.book_id%Type,p_sell_book_count number);
+procedure sell(p_store_id available_book.store_id%type,p_book_id available_book.book_id%Type,p_sell_book_count number);--extra procedure
 end available_book_pkg;
 /
 create or replace package body available_book_pkg as
 
 procedure ins(p_book_id available_book.book_id%type,p_store_id available_book.store_id%type) as
---v_cnt number;
---v_date date;
-
 begin
 
 if p_book_id is null or p_store_id is null  then
 log_pkg.log(p_message =>'Butun melumatlar daxil edilmelidir!!!',p_message_type =>'critical' );
 raise_application_error(-20001,'Butun melumatlar daxil edilmelidir!!!');
 end if;
-
-
-/*select count(*) into v_cnt from available_book where book_id =p_book_id and store_id=p_store_id and sold_date=p_sold_date;
-if v_cnt>0 then
-log_pkg.log(p_message =>'Kitab adi,magazani ve satilma tarixini tekrar daxil etmek olmaz!!!',p_message_type =>'noncritical' );
-raise_application_error(-20001,'Kitab adi,magazani ve satilma tarixini tekrar daxil etmek olmaz!!!');
-end if;*/
-
-
-insert into available_book(book_id,store_id,sold) values(p_book_id,p_store_id,0);
+insert into available_book(book_id,store_id,sold) values(p_book_id,p_store_id,0);--when you wish to enter any book to show-case then, sold=0,sold_date is null
 commit;
 end ins;
 
-/*procedure upd(p_book_id available_book.book_id%type,p_store_id available_book.store_id%Type,p_sold available_book.sold%Type,p_sold_date available_book.sold_date%Type) is
-v_cnt number;
-begin
-if p_book_id is null or p_store_id is null or p_sold is null or p_sold_date is null  then
-log_pkg.log(p_message =>'Butun melumatlar daxil edilmelidir!!!',p_message_type =>'critical');
-raise_application_error(-20001,'Butun melumatlar daxil edilmelidir!!!');
-end if;
-
-select count(*) into v_cnt from available_book where book_id=p_book_id and store_id=p_store_id and sold_date=p_sold_date;
-if v_cnt>0 then
-log_pkg.log(p_message =>'Bu melumatlar artiq movcuddur!!!',p_message_type =>'noncritical');
-raise_application_error(-20001,'Bu melumatlar artiq movcuddur!!!');
-end if;
-update  available_book set sold=p_sold where book_id=p_book_id and store_id=p_store_id and sold_date=p_sold_date;
-commit;
-end upd;
-*/
-
 procedure del(p_book_id available_book.book_id%type,p_store_id available_book.store_id%type,p_all_books boolean default false) as
 v_cnt number;
---v_sold number;
 begin
 if p_book_id is null or p_store_id is null then
 log_pkg.log(p_message =>'Melumatlari daxil edin!!!',p_message_type =>'critical');
@@ -63,25 +31,24 @@ log_pkg.log(p_message =>'Bu melumatlar movcud deyil!!!',p_message_type =>'noncri
 raise_application_error(-20001,'Bu melumatlar movcud deyil!!!');
 end if;
 if p_all_books!=true then 
- delete from available_book where sold=0 and book_id=p_book_id and store_id=p_store_id;
+ delete from available_book where sold=0 and book_id=p_book_id and store_id=p_store_id;--remove from exact branch(store)
 else
- delete from available_book where sold=0 and book_id=p_book_id;
+ delete from available_book where sold=0 and book_id=p_book_id;--remove from all branchs
 end if; 
---delete from available_book where sold=0 and book_id=p_book_id and    (false or  p_all_books );
 commit;
 end del;
 
 procedure sell(p_store_id available_book.store_id%type,p_book_id available_book.book_id%Type,p_sell_book_count number) is
  v_cnt number;
- coll tcoll:=tcoll();
+ coll tcoll:=tcoll();--after create nested table,intialize it
 begin
   select count(*) into v_cnt from available_book where store_id=p_store_id and book_id=p_book_id and sold=0 and sold_date is null;
   if v_cnt<p_sell_book_count then
     raise_application_error(-20001,'Not enough book available in store');
   end if;
   
-  select rowid bulk collect into coll from available_book where  sold=0 and sold_date is null  FETCH FIRST p_sell_book_count ROWS ONLY;
-  update available_book  set sold=1, sold_date=sysdate where rowid in (select * from table(coll));
+  select rowid bulk collect into coll from available_book where  sold=0 and sold_date is null  FETCH FIRST p_sell_book_count ROWS ONLY;--bulk collect & select first n rows only
+  update available_book  set sold=1,sold_date=sysdate where rowid in (select * from table(coll));--and update the rows which retrived  by collection
   commit;
 end sell;
 
